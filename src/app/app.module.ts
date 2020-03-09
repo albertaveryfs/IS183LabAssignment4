82fdc7874f54e27357bb06207b90eacbb8e65547
Albert Avery <albertavery@mail.fresnostate.edu>
2020-03-09 14:09:10 -0700

    added pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ArtistComponent } from './artist/artist.component';
+import { ArtworksComponent } from './artworks/artworks.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { routerModule } from './routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ArtistComponent,
+    ArtworksComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    routerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
